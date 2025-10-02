77ccd474fbb0304dca33836d25a0c50e4d7c883b
hliblack <hliblack@outlook.com>
2025-10-02 18:18:29 +0800

    删除python和shell文件

--- Shell/dert.sh
+++
@@ -1,9 +0,0 @@
-#提前把证书文件放入到"/etc/certs/"目录下
-
-docker run --restart always \
-  --name derper -p 443:12345 -p 3478:3478/udp \
-  -v /data/certs/:/app/certs \
-  -e DERP_CERT_MODE=manual \
-  -e DERP_ADDR=:443 \
-  -e DERP_DOMAIN=dert.hyouka.top \
-  -d ghcr.io/yangchuansheng/derper:latest
